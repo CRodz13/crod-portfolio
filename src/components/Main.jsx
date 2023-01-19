200e02405958d6769811980e7bac2ba9bb700dbe
Chris <78927284+CRodz13@users.noreply.github.com>
2023-01-19 18:32:28 -0500

    Version Control Test

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
     <div id='main'>
         <img className="w-full h-screen scale-x-[-1]" src={headshotImg} alt="" />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/25">
-            <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+            <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center fade-in'>
                 <h1 className='sm:text-5xl text-4xl font-bold shadow-xl text-gray-800 transform'>I'm Chris Rodriguez</h1>
                 <h2 className='flex sm:text-3xl text-2xl shadow-xl pt-4 text-gray-800'>I'm a
                     <TypeAnimation
