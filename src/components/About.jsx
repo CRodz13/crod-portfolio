000d75ac43f5c0cb68ed8cac2216c574e8e6651c
Chris <rodcee13@gmail.com>
2023-01-20 11:25:42 -0500

    aos about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -152,28 +152,34 @@ const About = () => {
         </p>
 
         <br />
-        <div
-    >
-      <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
-          <p className="text-2xl py-6 text-center">These are some of the technologies I work with</p>
-        </div>
-
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {techs.map(({ id, src, title, style }) => (
+          <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
             <div
-              key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+              data-aos="zoom-in-down"
+              data-aos-duration="1300"
+              data-aos-delay="400"
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
-              <p className="mt-4">{title}</p>
+              <p className="text-2xl py-6 text-center">
+                These are some of the technologies I work with
+              </p>
             </div>
-          ))}
+
+            <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0" data-aos="zoom-in-up"
+              data-aos-duration="1300"
+              data-aos-delay="400">
+              {techs.map(({ id, src, title, style }) => (
+                <div
+                  key={id}
+                  className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                >
+                  <img src={src} alt="" className="w-20 mx-auto" />
+                  <p className="mt-4">{title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-      </div>
-      
     </div>
   );
 };
